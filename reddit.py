61749b1515dbe9cc1079d4800424490ef7233068
Zeno McDohl <zenomcdohl@gmail.com>
2020-05-19 23:35:14 -0400

    Fixes #21

--- reddit.py
+++ reddit.py
@@ -1,5 +1,8 @@
 ### This file creates the instance of Reddit and authenticates ###
 
 import praw
+import sys
 
-reddit = praw.Reddit(user_agent='SyncCompanion bot')
+sub = sys.argv[1]
+
+reddit = praw.Reddit(sub, user_agent='SyncCompanion bot')
